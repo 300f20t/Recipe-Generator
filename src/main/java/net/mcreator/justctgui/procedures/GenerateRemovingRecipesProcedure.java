e8d97eb8cad63840043c8bff8eeb38f78aef4072
300f20t <93136063+300f20t@users.noreply.github.com>
2024-06-19 22:38:44 +0300

    fix code

--- src/main/java/net/mcreator/justctgui/procedures/GenerateRemovingRecipesProcedure.java
+++ src/main/java/net/mcreator/justctgui/procedures/GenerateRemovingRecipesProcedure.java
@@ -1,6 +1,20 @@
 package net.mcreator.justctgui.procedures;
 
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+
+import java.util.function.Supplier;
+import java.util.Map;
+
 public class GenerateRemovingRecipesProcedure {
-	public static void execute() {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (!((entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY).getItem() == Blocks.AIR.asItem())) {
+		} else {
+		}
 	}
 }
--- src/main/java/net/mcreator/justctgui/procedures/GenerateracipesProcedure.java
+++ src/main/java/net/mcreator/justctgui/procedures/GenerateracipesProcedure.java
@@ -2,7 +2,6 @@ package net.mcreator.justctgui.procedures;
 
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.network.chat.Component;
-import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.Checkbox;
 
 import net.mcreator.justctgui.network.JustCtguiModVariables;
@@ -13,17 +12,7 @@ public class GenerateracipesProcedure {
 	public static void execute(LevelAccessor world, HashMap guistate) {
 		if (guistate == null)
 			return;
-		if (guistate.containsKey("checkbox:Is_shapeless") && ((Checkbox) guistate.get("checkbox:Is_shapeless")).selected()) {
-			JustCtguiModVariables.Is_shapeless = true;
-		} else {
-			JustCtguiModVariables.Is_shapeless = false;
-		}
-		if (guistate.containsKey("checkbox:Is_mirrored") && ((Checkbox) guistate.get("checkbox:Is_mirrored")).selected()) {
-			JustCtguiModVariables.Is_mirrored = true;
-		} else {
-			JustCtguiModVariables.Is_mirrored = false;
-		}
-		JustCtguiModVariables.Pre_generated_recipe = JustCtguiModVariables.Is_shapeless
+		JustCtguiModVariables.Pre_generated_recipe = guistate.containsKey("checkbox:Is_shapeless") && ((Checkbox) guistate.get("checkbox:Is_shapeless")).selected()
 				? (JustCtguiModVariables.item_in_slot_9_crafting_table + ", [" + JustCtguiModVariables.item_in_slot_0_crafting_table + ", " + JustCtguiModVariables.item_in_slot_1_crafting_table + ", "
 						+ JustCtguiModVariables.item_in_slot_2_crafting_table + ", " + JustCtguiModVariables.item_in_slot_3_crafting_table + ", " + JustCtguiModVariables.item_in_slot_4_crafting_table + ", "
 						+ JustCtguiModVariables.item_in_slot_5_crafting_table + ", " + JustCtguiModVariables.item_in_slot_6_crafting_table + ", " + JustCtguiModVariables.item_in_slot_7_crafting_table + ", "
@@ -32,22 +21,13 @@ public class GenerateracipesProcedure {
 						+ JustCtguiModVariables.item_in_slot_2_crafting_table + "], [" + JustCtguiModVariables.item_in_slot_3_crafting_table + ", " + JustCtguiModVariables.item_in_slot_4_crafting_table + ", "
 						+ JustCtguiModVariables.item_in_slot_5_crafting_table + "], [" + JustCtguiModVariables.item_in_slot_6_crafting_table + ", " + JustCtguiModVariables.item_in_slot_7_crafting_table + ", "
 						+ JustCtguiModVariables.item_in_slot_8_crafting_table + "]]);";
-		if ((guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "").isEmpty() && !JustCtguiModVariables.Is_shapeless && !JustCtguiModVariables.Is_mirrored) {
-			JustCtguiModVariables.Generated_recipe = "craftingTable.addShaped(\"" + "no_name" + new java.text.DecimalFormat("##.##").format(JustCtguiModVariables.generated_count) + "\", " + JustCtguiModVariables.Pre_generated_recipe;
-		} else if (!JustCtguiModVariables.Is_shapeless && !JustCtguiModVariables.Is_mirrored) {
-			JustCtguiModVariables.Generated_recipe = "craftingTable.addShaped(\"" + "" + (guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "") + "\", " + JustCtguiModVariables.Pre_generated_recipe;
-		} else if ((guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "").isEmpty() && JustCtguiModVariables.Is_shapeless) {
-			JustCtguiModVariables.Generated_recipe = "craftingTable.addShapeless(\"" + "no_name" + new java.text.DecimalFormat("##.##").format(JustCtguiModVariables.generated_count) + "\", " + JustCtguiModVariables.Pre_generated_recipe;
-		} else if (JustCtguiModVariables.Is_shapeless) {
-			JustCtguiModVariables.Generated_recipe = "craftingTable.addShapeless(\"" + "" + (guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "") + "\", "
-					+ JustCtguiModVariables.Pre_generated_recipe;
-		} else if ((guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "").isEmpty() && JustCtguiModVariables.Is_mirrored) {
-			JustCtguiModVariables.Generated_recipe = "import crafttweaker.api.recipe.MirrorAxis;" + "craftingTable.addShapedMirrored(\"" + "no_name" + new java.text.DecimalFormat("##.##").format(JustCtguiModVariables.generated_count) + "\", "
-					+ "MirrorAxis." + JustCtguiModVariables.Mirror_axis + ", " + JustCtguiModVariables.Pre_generated_recipe;
-		} else if (JustCtguiModVariables.Is_mirrored) {
-			JustCtguiModVariables.Generated_recipe = "import crafttweaker.api.recipe.MirrorAxis;" + "craftingTable.addShapedMirrored(\"" + (guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "")
-					+ "\", " + "MirrorAxis." + JustCtguiModVariables.Mirror_axis + ", " + JustCtguiModVariables.Pre_generated_recipe;
-		}
+		JustCtguiModVariables.Generated_recipe = (guistate.containsKey("checkbox:Is_mirrored") && ((Checkbox) guistate.get("checkbox:Is_mirrored")).selected() ? "import crafttweaker.api.recipe.MirrorAxis;" + "craftingTable.addShapedMirrored(\"" : "")
+				+ "" + (guistate.containsKey("checkbox:Is_shapeless") && ((Checkbox) guistate.get("checkbox:Is_shapeless")).selected() ? "craftingTable.addShapeless(\"" : "")
+				+ (!(guistate.containsKey("checkbox:Is_mirrored") && ((Checkbox) guistate.get("checkbox:Is_mirrored")).selected() || guistate.containsKey("checkbox:Is_shapeless") && ((Checkbox) guistate.get("checkbox:Is_shapeless")).selected())
+						? "craftingTable.addShaped(\""
+						: "")
+				+ ((JustCtguiModVariables.Recipe_name).equals("\"\"") ? "no_name" : JustCtguiModVariables.Recipe_name) + "\", "
+				+ (guistate.containsKey("checkbox:Is_mirrored") && ((Checkbox) guistate.get("checkbox:Is_mirrored")).selected() ? JustCtguiModVariables.Mirror_axis + ", " : "") + JustCtguiModVariables.Pre_generated_recipe;
 		if (!world.isClientSide() && world.getServer() != null)
 			world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(JustCtguiModVariables.Generated_recipe), false);
 	}
