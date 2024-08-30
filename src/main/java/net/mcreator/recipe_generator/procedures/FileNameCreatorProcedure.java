0353ddba8aed7012ff8cd4c11c4746d288fe1cd8
BuildTools <93136063+300f20t@users.noreply.github.com>
2024-08-31 01:03:34 +0300

    fix name creating

--- src/main/java/net/mcreator/recipe_generator/procedures/FileNameCreatorProcedure.java
+++ src/main/java/net/mcreator/recipe_generator/procedures/FileNameCreatorProcedure.java
@@ -1,28 +1,17 @@
 package net.mcreator.recipe_generator.procedures;
 
-import net.neoforged.fml.loading.FMLPaths;
-
 import net.minecraft.client.gui.components.EditBox;
 
-import net.mcreator.recipe_generator.network.RecipeGeneratorModVariables;
-
 import java.util.HashMap;
-
-import java.io.File;
+import java.util.Calendar;
 
 public class FileNameCreatorProcedure {
 	public static String execute(HashMap guistate) {
 		if (guistate == null)
 			return "";
-		File oldGenerated = new File("");
 		String fileName = "";
 		if ((guistate.containsKey("text:file_name") ? ((EditBox) guistate.get("text:file_name")).getValue() : "").equals("")) {
-			oldGenerated = new File((FMLPaths.GAMEDIR.get().toString() + "/scripts"), File.separator + ("generated " + RecipeGeneratorModVariables.filesWithGeneratedNameCount + ".zs"));
-			while (oldGenerated.exists()) {
-				RecipeGeneratorModVariables.filesWithGeneratedNameCount = RecipeGeneratorModVariables.filesWithGeneratedNameCount + 1;
-				oldGenerated = new File((FMLPaths.GAMEDIR.get().toString() + "/scripts"), File.separator + ("generated " + RecipeGeneratorModVariables.filesWithGeneratedNameCount + ".zs"));
-			}
-			fileName = "generated " + new java.text.DecimalFormat("").format(RecipeGeneratorModVariables.filesWithGeneratedNameCount);
+			fileName = "generated " + new java.text.SimpleDateFormat("yyyy-MM-dd-hh-mm-ss").format(Calendar.getInstance().getTime());
 		} else {
 			fileName = guistate.containsKey("text:file_name") ? ((EditBox) guistate.get("text:file_name")).getValue() : "";
 		}
