b876b62a6b3daf5f9c2ae8bf013e34b25d770c86
300f20t <93136063+300f20t@users.noreply.github.com>
2024-10-10 01:39:18 +0300

    fix

--- src/main/java/net/mcreator/recipe_generator/procedures/ScriptswriterProcedure.java
+++ src/main/java/net/mcreator/recipe_generator/procedures/ScriptswriterProcedure.java
@@ -1,11 +1,63 @@
 package net.mcreator.recipe_generator.procedures;
 
+import net.neoforged.fml.loading.FMLPaths;
+
+import net.mcreator.recipe_generator.network.RecipeGeneratorModVariables;
+
 import java.util.HashMap;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.BufferedWriter;
 
 public class ScriptswriterProcedure {
 	public static void execute(HashMap guistate) {
 		if (guistate == null)
 			return;
+		String fileName = "";
+		File generated = new File("");
 		FileNameCreatorProcedure.execute(guistate);
+		generated = new File((FMLPaths.GAMEDIR.get().toString() + "/scripts"), File.separator + (FileNameCreatorProcedure.execute(guistate) + ".zs"));
+		if (!generated.exists()) {
+			try {
+				generated.getParentFile().mkdirs();
+				generated.createNewFile();
+			} catch (IOException exception) {
+				exception.printStackTrace();
+			}
+			try {
+				FileWriter generatedwriter = new FileWriter(generated, false);
+				BufferedWriter generatedbw = new BufferedWriter(generatedwriter);
+				{
+					generatedbw.write("import crafttweaker.api.recipe.MirrorAxis;");
+					generatedbw.newLine();
+				}
+				{
+					generatedbw.write("import crafttweaker.api.recipe.FurnaceRecipeManager;");
+					generatedbw.newLine();
+				}
+				{
+					generatedbw.write("import crafttweaker.api.recipe.BlastFurnaceRecipeManager;");
+					generatedbw.newLine();
+				}
+				generatedbw.close();
+				generatedwriter.close();
+			} catch (IOException exception) {
+				exception.printStackTrace();
+			}
+		}
+		try {
+			FileWriter generatedwriter = new FileWriter(generated, false);
+			BufferedWriter generatedbw = new BufferedWriter(generatedwriter);
+			{
+				generatedbw.write(RecipeGeneratorModVariables.Generated_recipe);
+				generatedbw.newLine();
+			}
+			generatedbw.close();
+			generatedwriter.close();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
 	}
 }
