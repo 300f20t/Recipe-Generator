70313bdb2c14014978884667d3a1945b44e510b7
300f20t <93136063+300f20t@users.noreply.github.com>
2024-12-01 01:51:24 +0300

    fix

--- src/main/java/net/mcreator/recipe_generator/procedures/ScriptswriterProcedure.java
+++ src/main/java/net/mcreator/recipe_generator/procedures/ScriptswriterProcedure.java
@@ -10,7 +10,10 @@ import net.mcreator.recipe_generator.network.RecipeGeneratorModVariables;
 
 import java.util.HashMap;
 
+import java.io.IOException;
+import java.io.FileWriter;
 import java.io.File;
+import java.io.BufferedWriter;
 
 public class ScriptswriterProcedure {
 	public static void execute(Entity entity, HashMap guistate) {
@@ -20,15 +23,43 @@ public class ScriptswriterProcedure {
 		FileNameCreatorProcedure.execute(guistate);
 		if ((RecipeGeneratorModVariables.selectedMethod).equals("CraftTweaker")) {
 			generated = new File((FMLPaths.GAMEDIR.get().toString() + "scripts/"), File.separator + (FileNameCreatorProcedure.execute(guistate) + ".zs"));
-			{
-				generated.write(RecipeGeneratorModVariables.Generated_recipe);
-				generated.newLine();
+			if (generated.exists()) {
+				try {
+					generated.getParentFile().mkdirs();
+					generated.createNewFile();
+				} catch (IOException exception) {
+					exception.printStackTrace();
+				}
+			}
+			try {
+				FileWriter generatedwriter = new FileWriter(generated, false);
+				BufferedWriter generatedbw = new BufferedWriter(generatedwriter);
+				generatedbw.write(RecipeGeneratorModVariables.Generated_recipe);
+				generatedbw.newLine();
+				generatedbw.close();
+				generatedwriter.close();
+			} catch (IOException exception) {
+				exception.printStackTrace();
 			}
 		} else if ((RecipeGeneratorModVariables.selectedMethod).equals("KubeJS")) {
 			generated = new File((FMLPaths.GAMEDIR.get().toString() + "server_scripts/"), File.separator + (FileNameCreatorProcedure.execute(guistate) + ".zs"));
-			{
-				generated.write(RecipeGeneratorModVariables.Generated_recipe);
-				generated.newLine();
+			if (generated.exists()) {
+				try {
+					generated.getParentFile().mkdirs();
+					generated.createNewFile();
+				} catch (IOException exception) {
+					exception.printStackTrace();
+				}
+			}
+			try {
+				FileWriter generatedwriter = new FileWriter(generated, false);
+				BufferedWriter generatedbw = new BufferedWriter(generatedwriter);
+				generatedbw.write(RecipeGeneratorModVariables.Generated_recipe);
+				generatedbw.newLine();
+				generatedbw.close();
+				generatedwriter.close();
+			} catch (IOException exception) {
+				exception.printStackTrace();
 			}
 		} else {
 			if (entity instanceof Player _player && !_player.level().isClientSide())
