f66bbfde61124f6904d5543d288f13c84be07846
300f20t <93136063+300f20t@users.noreply.github.com>
2024-11-30 21:04:00 +0300

    Update RecipeNameCreatorProcedure.java

--- src/main/java/net/mcreator/recipe_generator/procedures/RecipeNameCreatorProcedure.java
+++ src/main/java/net/mcreator/recipe_generator/procedures/RecipeNameCreatorProcedure.java
@@ -1,7 +1,21 @@
 package net.mcreator.recipe_generator.procedures;
 
+import net.minecraft.client.gui.components.EditBox;
+
+import net.mcreator.recipe_generator.network.RecipeGeneratorModVariables;
+
+import java.util.HashMap;
+import java.util.Calendar;
+
 public class RecipeNameCreatorProcedure {
-	public static String execute() {
-		return "generated";
+	public static String execute(HashMap guistate) {
+		if (guistate == null)
+			return "";
+		String fileName = "";
+		RecipeGeneratorModVariables.Recipe_name = guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "";
+		if ((RecipeGeneratorModVariables.Recipe_name).isEmpty()) {
+			RecipeGeneratorModVariables.Recipe_name = "generated " + new java.text.SimpleDateFormat("yyyy-MM-dd-hh-mm-ss").format(Calendar.getInstance().getTime());
+		}
+		return RecipeGeneratorModVariables.Recipe_name;
 	}
 }
