e8697b8ec5a7588e626833b36b0f45817db0b95a
300f20t <93136063+300f20t@users.noreply.github.com>
2024-12-12 01:42:42 +0300

    add Kube JS Generator

--- src/main/java/net/mcreator/recipe_generator/procedures/GenerateracipesProcedure.java
+++ src/main/java/net/mcreator/recipe_generator/procedures/GenerateracipesProcedure.java
@@ -16,27 +16,26 @@ public class GenerateracipesProcedure {
 		String preGeneratedRecipe = "";
 		String recipeType = "";
 		String recipeTypeFutures = "";
-		preGeneratedRecipe = RecipeGeneratorModVariables.item_in_slot_9_crafting_table + ", [[" + RecipeGeneratorModVariables.item_in_slot_0_crafting_table + ", " + RecipeGeneratorModVariables.item_in_slot_1_crafting_table + ", "
-				+ RecipeGeneratorModVariables.item_in_slot_2_crafting_table + "], [" + RecipeGeneratorModVariables.item_in_slot_3_crafting_table + ", " + RecipeGeneratorModVariables.item_in_slot_4_crafting_table + ", "
-				+ RecipeGeneratorModVariables.item_in_slot_5_crafting_table + "], [" + RecipeGeneratorModVariables.item_in_slot_6_crafting_table + ", " + RecipeGeneratorModVariables.item_in_slot_7_crafting_table + ", "
-				+ RecipeGeneratorModVariables.item_in_slot_8_crafting_table + "]]);";
-		if (guistate.containsKey("checkbox:Is_mirrored") && ((Checkbox) guistate.get("checkbox:Is_mirrored")).selected()) {
-			recipeType = "craftingTable.addShapedMirrored(\"";
-			recipeTypeFutures = "MirrorAxis." + RecipeGeneratorModVariables.Mirror_axis + ", ";
-		} else if (guistate.containsKey("checkbox:Is_shapeless") && ((Checkbox) guistate.get("checkbox:Is_shapeless")).selected()) {
-			recipeType = "craftingTable.addShapeless(\"";
-			recipeTypeFutures = preGeneratedRecipe.replace("<item:minecraft:air>, ", "");
-		} else {
-			recipeType = "craftingTable.addShaped(\"";
-			recipeTypeFutures = preGeneratedRecipe;
-		}
 		if ((RecipeGeneratorModVariables.WorldVariables.get(world).selectedMethod).equals("CraftTweaker")) {
+			preGeneratedRecipe = preGeneratedRecipe + ", [[" + preGeneratedRecipe + ", " + preGeneratedRecipe + ", " + preGeneratedRecipe + "], [" + preGeneratedRecipe + ", " + preGeneratedRecipe + ", " + preGeneratedRecipe + "], ["
+					+ preGeneratedRecipe + ", " + preGeneratedRecipe + ", " + preGeneratedRecipe + "]]);";
+			if (guistate.containsKey("checkbox:Is_mirrored") && ((Checkbox) guistate.get("checkbox:Is_mirrored")).selected()) {
+				recipeType = "craftingTable.addShapedMirrored(\"";
+				recipeTypeFutures = "MirrorAxis." + RecipeGeneratorModVariables.Mirror_axis + ", ";
+			} else if (guistate.containsKey("checkbox:Is_shapeless") && ((Checkbox) guistate.get("checkbox:Is_shapeless")).selected()) {
+				recipeType = "craftingTable.addShapeless(\"";
+				recipeTypeFutures = preGeneratedRecipe.replace("<item:minecraft:air>, ", "");
+			} else {
+				recipeType = "craftingTable.addShaped(\"";
+				recipeTypeFutures = preGeneratedRecipe;
+			}
 			RecipeGeneratorModVariables.Generated_recipe = recipeType + ""
 					+ ((guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : "").isEmpty()
 							? "no_name"
 							: (guistate.containsKey("text:recipe_name") ? ((EditBox) guistate.get("text:recipe_name")).getValue() : ""))
 					+ "\", " + recipeTypeFutures;
 		} else if ((RecipeGeneratorModVariables.WorldVariables.get(world).selectedMethod).equals("KubeJS")) {
+			RecipeGeneratorModVariables.Generated_recipe = "ServerEvents.recipes(event => {" + "event.shaped(" + "Item.of(" + ")" + "})";
 		}
 		if (!world.isClientSide() && world.getServer() != null)
 			world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(RecipeGeneratorModVariables.Generated_recipe), false);
