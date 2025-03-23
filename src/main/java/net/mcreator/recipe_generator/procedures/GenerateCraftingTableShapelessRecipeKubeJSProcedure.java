9c8e237fec2190602be652e3d1b5a3f90d7e44d3
300f20t <93136063+300f20t@users.noreply.github.com>
2025-03-23 23:37:35 +0300

    fix

--- src/main/java/net/mcreator/recipe_generator/procedures/GenerateCraftingTableShapelessRecipeKubeJSProcedure.java
+++ src/main/java/net/mcreator/recipe_generator/procedures/GenerateCraftingTableShapelessRecipeKubeJSProcedure.java
@@ -7,11 +7,13 @@ public class GenerateCraftingTableShapelessRecipeKubeJSProcedure {
 		String preGeneratedRecipe = "";
 		String recipeType = "";
 		String recipeTypeFutures = "";
-		double i = 0;
-		preGeneratedRecipe = "\n" + (RecipeGeneratorModVariables.item_in_slot_0 + "," + "\n") + (RecipeGeneratorModVariables.item_in_slot_1 + "," + "\n") + (RecipeGeneratorModVariables.item_in_slot_2 + "," + "\n")
-				+ (RecipeGeneratorModVariables.item_in_slot_3 + "," + "\n") + (RecipeGeneratorModVariables.item_in_slot_4 + "," + "\n") + (RecipeGeneratorModVariables.item_in_slot_5 + "," + "\n")
-				+ (RecipeGeneratorModVariables.item_in_slot_6 + "," + "\n") + (RecipeGeneratorModVariables.item_in_slot_7 + "," + "\n") + (RecipeGeneratorModVariables.item_in_slot_8 + "," + "\n");
-		RecipeGeneratorModVariables.Generated_recipe = "ServerEvents.recipes(event => {" + "event.shapeless(" + "\n" + " Item.of(" + RecipeGeneratorModVariables.item_in_slot_9 + ", "
-				+ new java.text.DecimalFormat("##").format(RecipeGeneratorModVariables.item_in_slot_9_count) + ")," + "\n" + " [" + "\n" + " ]," + "\n" + " {" + preGeneratedRecipe + "})})";
+		preGeneratedRecipe = ((RecipeGeneratorModVariables.item_in_slot_0).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_0 + ",") + ""
+				+ ((RecipeGeneratorModVariables.item_in_slot_1).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_1 + ",")
+				+ ((RecipeGeneratorModVariables.item_in_slot_2).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_2 + ",")
+				+ ((RecipeGeneratorModVariables.item_in_slot_3).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_3 + ",")
+				+ ((RecipeGeneratorModVariables.item_in_slot_4).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_5 + ",")
+				+ ((RecipeGeneratorModVariables.item_in_slot_6).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_6 + ",")
+				+ ((RecipeGeneratorModVariables.item_in_slot_7).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_7 + ",")
+				+ ((RecipeGeneratorModVariables.item_in_slot_8).equals("'minecraft:air'") ? "" : RecipeGeneratorModVariables.item_in_slot_8 + ",");
 	}
 }
