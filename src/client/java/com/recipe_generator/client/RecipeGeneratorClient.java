48e37b7f3814b9f1e1f05cb6189dedc34df9d7e2
300f20t <300f20t@gmail.com>
2026-05-19 16:30:51 +0300

    init

---
+++ src/client/java/com/recipe_generator/client/RecipeGeneratorClient.java
@@ -0,0 +1,10 @@
+package com.recipe_generator.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecipeGeneratorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/com/recipe_generator/RecipeGenerator.java
@@ -0,0 +1,18 @@
+package com.recipe_generator;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecipeGenerator implements ModInitializer {
+	public static final String MOD_ID = "recipe_generator";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Initializing Recipe Generator, no way...");
+	}
+}
