4935f27c6d26afcf052b41d0158943414b458f55
300f20t <93136063+300f20t@users.noreply.github.com>
2026-07-23 13:30:01 +0300

    Added custom output slot

---
+++ src/client/java/com/recipe_generator/client/CustomOutputSlot.java
@@ -0,0 +1,17 @@
+package com.recipe_generator.client;
+
+import net.minecraft.world.Container;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+
+public class CustomOutputSlot extends Slot {
+    
+    public CustomOutputSlot(Container container, int index, int x, int y) {
+        super(container, index, x, y);
+    }
+    
+    @Override
+    public boolean mayPlace(ItemStack stack) {
+        return this.getItem().isEmpty();
+    }
+}
