801297f68aecc0ac86b815af45b60d067cf902d8
300f20t <300f20t@gmail.com>
2026-08-30 18:41:31 +0300

    chore: change signature

--- neoforge/src/main/java/io/github/ftincdev/recipe_generator/client/RecipeGeneratorClient.java
+++ neoforge/src/main/java/io/github/ftincdev/recipe_generator/client/RecipeGeneratorClient.java
@@ -7,7 +7,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
-@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
 public class RecipeGeneratorClient {
 
     @SubscribeEvent
